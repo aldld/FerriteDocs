31656a194524ad3b44ffd92ee22c20589b62e7e0
Eric Bannatyne <eric.bannatyne@gmail.com>
2010-08-10 11:41:05 -0700

    Add config.php with blank settings

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+use ferrite\config\Config;
+
+Config::addValue('db_hostname', '');
+Config::addValue('db_database', '');
+Config::addValue('db_username', '');
+Config::addValue('db_password', '');
